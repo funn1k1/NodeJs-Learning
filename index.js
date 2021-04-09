1b2d0b75b3c80640254cd980865ebef2456567e9
funn1k1 <yukovich01@mail.ru>
2021-04-09 23:09:25 +0300

    add styles.css

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.end('Hello from node.js');
+    res.render('index');
 })
 
 app.listen(3000, () => {
